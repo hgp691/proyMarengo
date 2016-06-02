eb050e7833094f8fb9dbce6b42ce76007b4299da
hgp691 <root@MacBook-Pro-de-horacio.local>
2016-06-02 17:11:02 -0500

    edita install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
 #instalacion de archivos
+
+#creacion de las carpetas en usb que guardan los datos
+sudo mkdir /mnt/usb/config/
+sudo mkdir /mnt/usb/datos/
+sudo mkdir /mnt/usb/logs/
+
+#instalar requests
+cd /mnt/usb/ && git clone git://github.com/kennethreitz/requests.git
+cd /mnt/usb/requests/ && sudo python setup.py install -y
