67293e86346cafc482044e71bd4696aeb476ca8f
hgp691 <root@MacBook-Pro-de-horacio.local>
2016-06-02 17:44:26 -0500

    editar

--- install.sh
+++ install.sh
@@ -10,3 +10,13 @@ cd /mnt/usb/ && git clone git://github.com/kennethreitz/requests.git
 cd /mnt/usb/requests/ && sudo python setup.py install -y
 
 #copiar los archivos de inicio
+sudo cp /etc/rc.local /etc/rc.local.bup
+sudo rm /etc/rc.local
+sudo cp /sistema/rc.local /etc/rc.local
+
+sudo cp /etc/crontab /etc/crontab.bup
+sudo rm /etc/crontab
+sudo cp /sistema/crontab /etc/crontab
+
+#copiar los archivos de configuracion
+sudo cp /sistema/config/* /mnt/usb/config/ -r
